f8578cc094e68a36bf91a53c3140cbb1ca006da7
mtholcombe <matt@designengine.ai>
2019-11-16 19:29:24 -0800

    adding to placeholder gen to Images

--- lang/images.js
+++ lang/images.js
@@ -2,7 +2,7 @@
 
 
 const Images = {
-	dataURL : (img, { width, height }, enc='image/png')=> {
+	dataURL        : (img, { width, height }, enc='image/png')=> {
 		const canvas = document.createElement('canvas');
 		canvas.width = width;
 		canvas.height = height;
@@ -15,6 +15,36 @@ const Images = {
 
 		return (imgData);
 	},
+	genPlaceholder : ({ width, height }, bgColor='#999999', fgColor='rgb(31, 31, 31, 0.875')=> {
+		const canvas = document.createElement('canvas');
+		const context = canvas.getContext('2d');
+
+		document.body.appendChild(canvas);
+		canvas.width = width;
+		canvas.height = height;
+
+		context.clearRect(0, 0, canvas.width, canvas.height);
+		context.fillStyle = bgColor;
+		context.fillRect(0, 0, width, height);
+
+		context.lineWidth = 1.0;
+		context.setLineDash([3, 2]);
+		context.lineDashOffset = 0;
+		context.beginPath();
+		context.strokeStyle = fgColor; // #333333
+
+		context.beginPath();
+		context.moveTo(0, 0);
+		context.lineTo(width, height);
+		context.moveTo(width, 0);
+		context.lineTo(0, height);
+		context.stroke();
+
+		const dataURL = canvas.toDataURL();
+		canvas.remove();
+
+		return (dataURL);
+	}
 };
 
 
