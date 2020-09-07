07f3d7d1b36042cac3b3f9dcf52cfb334e112885
mtholcombe <matt@pairurl.com>
2020-09-07 16:53:47 -0700

    adding copy to clipboard + user agent

--- lang/browsers.js
+++ lang/browsers.js
@@ -3,7 +3,7 @@
 
 const Browsers = (typeof navigator !== 'undefined') ? {
 	clipboardCopy : (str)=> {
-// 		navigator.clipboard.writeText(str);
+		// 		navigator.clipboard.writeText(str);
 		const txtArea = document.createElement('textarea');
 		txtArea.innerText = str;
 		document.body.appendChild(txtArea);
@@ -11,8 +11,9 @@ const Browsers = (typeof navigator !== 'undefined') ? {
 		document.execCommand('copy');
 		txtArea.remove();
 	},
-	isOnline     : ()=> (navigator.onLine),
-	isMobile     : {
+	clipboardText : ()=> (navigator.clipboard.readText().then((text)=> (text))),
+	isOnline      : ()=> (navigator.onLine),
+	isMobile      : {
 		Android    : ()=> (/Android/i.test(navigator.userAgent)),
 		BlackBerry : ()=> (/BlackBerry/i.test(navigator.userAgent)),
 		iOS        : ()=> (/iPhone|iPad|iPod/i.test(navigator.userAgent)),
@@ -22,8 +23,8 @@ const Browsers = (typeof navigator !== 'undefined') ? {
 		Windows    : ()=> (/IEMobile|WPDesktop/i.test(navigator.userAgent)),
 		ANY        : ()=> (Browsers.isMobile.Android() || Browsers.isMobile.iOS() || Browsers.isMobile.Windows() || Browsers.isMobile.Opera() || Browsers.isMobile.BlackBerry())
 	},
-	isSafari     : ()=> (/Mac OS X.+Safari/i).test(navigator.userAgent && !/Mac OS X.+Chrome/i.test(navigator.userAgent)),
-	makeDownload : (url, blank=false)=> {
+	isSafari      : ()=> (/Mac OS X.+Safari/i).test(navigator.userAgent && !/Mac OS X.+Chrome/i.test(navigator.userAgent)),
+	makeDownload  : (url, blank=false)=> {
 		let link = document.createElement('a');
 		link.target = (blank) ? '_blank' : '_self';
 		link.href = url;
@@ -32,13 +33,12 @@ const Browsers = (typeof navigator !== 'undefined') ? {
 		link.click();
 		link.remove();
 	},
-	scrollElement : (element, coords={ x : 0, y : 0 })=> {
+	scrollElement  : (element, coords={ x : 0, y : 0 })=> {
 		if (element) {
 			element.scrollTo(coords.x, coords.y);
 		}
 	},
-	scrollOrigin : (element)=> (Browsers.scrollElement(element)),
-	userAgent     : { navigator }
+	scrollOrigin   : (element)=> (Browsers.scrollElement(element))
 } : {};
 
 
--- lang/uris.js
+++ lang/uris.js
@@ -3,6 +3,14 @@
 import { Arrays, Files, Strings } from '../index';
 
 const URIs = {
+	extractURLs    : (val)=> {
+		const matches = val.match(new RegExp('(?:(?:https?|ftp)://)(?:\\S+(?::\\S*)?@)?(?:(?!(?:10|127)(?:\\.\\d{1,3}){3})(?!(?:169\\.254|192\\.168)(?:\\.\\d{1,3}){2})(?!172\\.(?:1[6-9]|2\\d|3[0-1])(?:\\.\\d{1,3}){2})(?:[1-9]\\d?|1\\d\\d|2[01]\\d|22[0-3])(?:\\.(?:1?\\d{1,2}|2[0-4]\\d|25[0-5])){2}(?:\\.(?:[1-9]\\d?|1\\d\\d|2[0-4]\\d|25[0-4]))|(?:(?:[a-z\\u00a1-\\uffff0-9]-*)*[a-z\\u00a1-\\uffff0-9]+)(?:\\.(?:[a-z\\u00a1-\\uffff0-9]-*)*[a-z\\u00a1-\\uffff0-9]+)*(?:\\.(?:[a-z\\u00a1-\\uffff]{2,})))(?::\\d{2,5})?(?:/\\S*)?', 'ig'));
+		if (!matches) {
+    return ([]);
+  }
+
+  return (matches.filter((value, index)=> (matches.indexOf(value) === index)));
+	},
 	firstComponent : (url=window.location.pathname, trim=true)=> ((trim) ? Strings.trimBounds(url, '/').split('/').shift() : url.substr(1).split('/').shift()),
 	hostname       : (url=window.location.hostname)=> (url.replace(/^https?:\/\//g, '').split('/').shift()),
 	lastComponent  : (url=window.location.pathname)=> (Files.basename(url)),
